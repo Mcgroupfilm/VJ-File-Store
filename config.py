05639e5a82ddb5b595179423db110312c89f9b6b
Mcgroupfilm <130358785+Mcgroupfilm@users.noreply.github.com>
2024-01-09 21:52:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -94,9 +94,9 @@ class Var(object):
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
-        URL = "https://mean-nesta-thirankumar.koyeb.app/"
+        URL = "https://bad-loree-thirankumar.koyeb.app/"
     else:
-        URL = "https://mean-nesta-thirankumar.koyeb.app/"
+        URL = "https://bad-loree-thirankumar.koyeb.app/"
 
 
 
